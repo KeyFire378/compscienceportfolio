8ed689a3ed56ba638f7d8b6cd4af4a9c54087f9e
KeyFire378 <85497788+KeyFire378@users.noreply.github.com>
2022-08-11 15:42:21 -0400

    Added js
    Trying to make about me  more centralized but is still buggy

---
+++ main.js
@@ -0,0 +1,11 @@
+function hidetxt() {
+    var more = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+    if (more.style.display === "inline") {
+        btnText.innerHTML = " \/ ";
+        more.style.display = "none";
+    } else {
+        btnText.innerHTML = " /\ ";
+        more.style.display = "inline";
+    }
+}
