50e1c0c0f7fef1b74d9bc7695d3a4eb8cd27e47e
KeyFire378 <85497788+KeyFire378@users.noreply.github.com>
2022-08-17 23:03:02 -0400

    Added a background div and arrow
    Made sure that there is now a collapsible tab in about me

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ function hidetxt() {
     var more = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
     if (more.style.display === "inline") {
-        btnText.innerHTML = " \/ ";
+        btnText.innerHTML = " \_/ ";
         more.style.display = "none";
     } else {
-        btnText.innerHTML = " /\ ";
+        btnText.innerHTML = " /-\ ";
         more.style.display = "inline";
     }
 }
