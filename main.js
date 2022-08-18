0fc00942b7bd03ce7b19c8a70ab018d4c6921251
KeyFire378 <85497788+KeyFire378@users.noreply.github.com>
2022-08-18 15:42:12 -0400

    Adjusted js
    Made arrows for both up and down and possibly fixed font discrepancies

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+// These are functions to solve an issue
+function fontfix() {
+    var movtext = document.getElementsByTagName("marquee");
+    var ptext = document.getElementsByTagName("p");
+    movtext.style.fontSize = ptext.style.fontSize;
+}
+// This is where the functions begin
 function hidetxt() {
     var more = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
     if (more.style.display === "inline") {
-        btnText.innerHTML = " \_/ ";
+        btnText.style.backgroundImage = "url(uparrow.svg)";
         more.style.display = "none";
     } else {
-        btnText.innerHTML = " /-\ ";
+        btnText.style.backgroundImage = "url(darrow.svg)";
         more.style.display = "inline";
     }
 }
