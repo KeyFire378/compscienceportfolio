4accae1e1bfa11eac4d8bfa2e3dc3b10fcdf5ccd
KeyFire378 <85497788+KeyFire378@users.noreply.github.com>
2022-11-24 19:11:28 -0500

    Fixed Padding
    Made sure that hiding text with the arrow button no longer makes padding inaccurate

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-// These are functions to solve an issue
-function fontfix() {
-    var movtext = document.getElementsByTagName("marquee");
-    var ptext = document.getElementsByTagName("p");
-    movtext.style.fontSize = ptext.style.fontSize;
-}
 // This is where the functions begin
 function hidetxt(stuff, btn) {
     var more = document.getElementById(stuff);
     var btnText = document.getElementById(btn);
-    if (more.style.display === "inline") {
+    if (more.style.display === "block") {
         btnText.style.backgroundImage = "url(uparrow.svg)";
         more.style.display = "none";
     } else {
         btnText.style.backgroundImage = "url(darrow.svg)";
-        more.style.display = "inline";
+        more.style.display = "block";
     }
 }
